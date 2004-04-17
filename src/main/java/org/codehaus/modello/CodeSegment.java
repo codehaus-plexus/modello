70e22590124a0e505e01f29c9333ab1a7c4a0a01
jvanzyl <jvanzyl@f0865d36-f2f9-0310-a560-dfd520db29d7>
2004-04-17 16:20:00 +0000

    o add missing source

---
+++ src/main/java/org/codehaus/modello/CodeSegment.java
@@ -0,0 +1,16 @@
+package org.codehaus.modello;
+
+/**
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @version $Id$
+ */
+public class CodeSegment
+    extends BaseElement
+{
+    private String code;
+
+    public String getCode()
+    {
+        return code;
+    }
+}
