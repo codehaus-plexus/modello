70e33b0c100a80fffa09c292231fb4cc24182501
jvanzyl <jvanzyl@f0865d36-f2f9-0310-a560-dfd520db29d7>
2004-06-06 05:50:26 +0000

    *** empty log message ***

--- modello/modello-core/src/main/java/org/codehaus/modello/Modello.java
+++ modello/modello-core/src/main/java/org/codehaus/modello/Modello.java
@@ -1,5 +1,6 @@
 package org.codehaus.modello;
 
+import org.codehaus.modello.generator.AbstractGenerator;
 import org.codehaus.modello.generator.java.JavaGenerator;
 import org.codehaus.modello.generator.xml.schema.XmlSchemaGenerator;
 import org.codehaus.modello.generator.xml.xdoc.XdocGenerator;
@@ -23,6 +24,8 @@ public class Modello
 
     private String modelVersion;
 
+    private boolean packageWithVersion;
+
     public Modello()
     {
     }
@@ -30,20 +33,25 @@ public class Modello
     public void parseArgumentsFromCommandLine( String[] args )
         throws Exception
     {
-        if ( args.length != 4 )
+        if ( args.length != 5 )
         {
             System.err.println( "Usage: modello <model> <mode> <output directory> " );
-            
+
             System.exit( 1 );
         }
 
-        setModel( args[0] );
+        model = args[0];
 
-        setMode( args[1] );
+        mode = args[1];
 
-        setOutputDirectory( args[2] );
+        outputDirectory = args[2];
 
-        setModelVersion( args[3] );
+        modelVersion = args[3];
+
+        if ( args[4].equals( "true" ) )
+        {
+            packageWithVersion = true;
+        }
     }
 
     public void work()
@@ -76,21 +84,41 @@ public class Modello
 
         if ( mode.equals( "java" ) )
         {
-            javaGenerator( model, outputDirectory, modelVersion );
+            JavaGenerator g = new JavaGenerator( model, new File( outputDirectory ).getPath(), modelVersion, packageWithVersion );
+
+            System.out.println( "Generating java in " + outputDirectory + " from " + model );
+
+            g.generate();
         }
         else if ( mode.equals( "xsd" ) )
         {
-            xmlSchemaGenerator( model, outputDirectory, modelVersion );
+            XmlSchemaGenerator generator = new XmlSchemaGenerator( model, new File( outputDirectory ).getPath(), modelVersion );
+
+            System.out.println( "Generating xml schema in " + outputDirectory + " from " + model );
+
+            generator.generate();
         }
         else if ( mode.equals( "xdoc" ) )
         {
-            xdocGenerator( model, outputDirectory, modelVersion );
+            XdocGenerator generator = new XdocGenerator( model, new File( outputDirectory ).getPath(), modelVersion );
+
+            System.out.println( "Generating xdoc in " + outputDirectory + " from " + model );
+
+            generator.generate();
         }
         else if ( mode.equals( "xpp3" ) )
         {
-            xpp3UnmarshallerGenerator( model, outputDirectory, modelVersion );
+            JavaGenerator generator = new Xpp3ReaderGenerator( model, new File( outputDirectory ).getPath(), modelVersion, packageWithVersion );
 
-            xpp3MarshallerGenerator( model, outputDirectory, modelVersion );
+            System.out.println( "Generating xpp3 unmarshaller in " + outputDirectory + " from " + model );
+
+            generator.generate();
+
+            generator = new Xpp3WriterGenerator( model, new File( outputDirectory ).getPath(), modelVersion, packageWithVersion );
+
+            System.out.println( "Generating xpp3 marshaller in " + outputDirectory + " from " + model );
+
+            generator.generate();
         }
     }
 
@@ -103,75 +131,4 @@ public class Modello
 
         modello.work();
     }
-
-    public void javaGenerator( String model, String outputDirectory, String modelVersion )
-        throws Exception
-    {
-        JavaGenerator generator = new JavaGenerator( model, new File( outputDirectory ).getPath(), modelVersion );
-
-        System.out.println( "Generating java in " + outputDirectory + " from " + model );
-
-        generator.generate();
-    }
-
-    public void xmlSchemaGenerator( String model, String outputDirectory, String modelVersion )
-        throws Exception
-    {
-        XmlSchemaGenerator generator = new XmlSchemaGenerator( model, new File( outputDirectory ).getPath(), modelVersion );
-
-        System.out.println( "Generating xml schema in " + outputDirectory + " from " + model );
-
-        generator.generate();
-    }
-
-    public void xdocGenerator( String model, String outputDirectory, String modelVersion )
-        throws Exception
-    {
-        XdocGenerator generator = new XdocGenerator( model, new File( outputDirectory ).getPath(), modelVersion );
-
-        System.out.println( "Generating xdoc in " + outputDirectory + " from " + model );
-
-        generator.generate();
-    }
-
-    public void xpp3UnmarshallerGenerator( String model, String outputDirectory, String modelVersion )
-        throws Exception
-    {
-        Xpp3ReaderGenerator generator = new Xpp3ReaderGenerator( model, new File( outputDirectory ).getPath(), modelVersion );
-
-        System.out.println( "Generating xpp3 unmarshaller in " + outputDirectory + " from " + model );
-
-        generator.generate();
-    }
-
-    public void xpp3MarshallerGenerator( String model, String outputDirectory, String modelVersion )
-        throws Exception
-    {
-        Xpp3WriterGenerator generator = new Xpp3WriterGenerator( model, new File( outputDirectory ).getPath(), modelVersion );
-
-        System.out.println( "Generating xpp3 marshaller in " + outputDirectory + " from " + model );
-
-        generator.generate();
-    }
-
-    public void setMode( String mode )
-    {
-        this.mode = mode;
-    }
-
-    public void setModel( String model )
-    {
-        this.model = model;
-    }
-
-    public void setOutputDirectory( String outputDirectory )
-    {
-        this.outputDirectory = outputDirectory;
-    }
-
-    public void setModelVersion( String modelVersion )
-    {
-        this.modelVersion = modelVersion;
-    }
-
 }
