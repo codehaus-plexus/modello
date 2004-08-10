ec3ebedbe6dfb357ac59fc4a34082c07f8124e0f
evenisse <evenisse@f0865d36-f2f9-0310-a560-dfd520db29d7>
2004-08-10 12:30:05 +0000

    Add default implementation

--- modello-core/src/main/java/org/codehaus/modello/metadata/AbstractMetaDataPlugin.java
+++ modello-core/src/main/java/org/codehaus/modello/metadata/AbstractMetaDataPlugin.java
@@ -8,6 +8,9 @@ import org.codehaus.modello.AbstractLogEnabled;
 import org.codehaus.modello.Model;
 import org.codehaus.modello.ModelClass;
 import org.codehaus.modello.ModelField;
+import org.codehaus.modello.ModelloException;
+
+import java.util.Map;
 
 /**
  * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
@@ -28,6 +31,24 @@ public abstract class AbstractMetaDataPlugin
     }
 
     public MetaData getFieldMetaData( ModelField field )
+    {
+        return null;
+    }
+
+    public Map getModelMap( Model model, MetaData metaData )
+        throws ModelloException
+    {
+        return null;
+    }
+
+    public Map getClassMap( ModelClass clazz, MetaData metaData )
+        throws ModelloException
+    {
+        return null;
+    }
+
+    public Map getFieldMap( ModelField field, MetaData metaData )
+        throws ModelloException
     {
         return null;
     }
