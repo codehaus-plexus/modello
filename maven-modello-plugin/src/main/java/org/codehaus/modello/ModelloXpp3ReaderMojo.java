8d55d9cc4abce5a34a94357eecd2c378a19746c9
brett <brett@f0865d36-f2f9-0310-a560-dfd520db29d7>
2005-03-09 04:19:41 +0000

    need a default for expression

--- maven-modello-plugin/src/main/java/org/codehaus/modello/ModelloXpp3ReaderMojo.java
+++ maven-modello-plugin/src/main/java/org/codehaus/modello/ModelloXpp3ReaderMojo.java
@@ -61,6 +61,7 @@ package org.codehaus.mojo.modello;
  * required="required"
  * validator=""
  * expression="#packageWithVersion"
+ * default="false"
  * description="True if the generated package names should include the version."
  *
  * @parameter name="modelloCore"
