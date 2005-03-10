f44962f9ad654ba7add449f60bc7d28c91fb7fb9
brett <brett@f0865d36-f2f9-0310-a560-dfd520db29d7>
2005-03-10 21:44:38 +0000

    allow specification of tag name on class metadata for root element

--- src/main/java/org/codehaus/modello/plugin/model/ModelClassMetadata.java
+++ src/main/java/org/codehaus/modello/plugin/model/ModelClassMetadata.java
@@ -35,6 +35,18 @@ public class ModelClassMetadata
 
     private boolean rootElement = false;
 
+    private String tagName;
+
+    public String getTagName()
+    {
+        return tagName;
+    }
+
+    public void setTagName( String tagName )
+    {
+        this.tagName = tagName;
+    }
+
     public boolean isRootElement()
     {
         return rootElement;
