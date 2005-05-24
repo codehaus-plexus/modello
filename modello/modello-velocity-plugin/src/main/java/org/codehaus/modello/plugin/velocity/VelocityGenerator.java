30d233f277b5bddf1a87a7aa565d06bf5825adc3
(no author) <(no author)@f0865d36-f2f9-0310-a560-dfd520db29d7>
2005-05-24 14:20:21 +0000

    This commit was manufactured by cvs2svn to create tag 'INIT'.

---
+++ modello/modello-velocity-plugin/src/main/java/org/codehaus/modello/plugin/velocity/VelocityGenerator.java
@@ -0,0 +1,90 @@
+package org.codehaus.modello.plugin.velocity;
+
+/*
+ * Copyright (c) 2004, Codehaus.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import org.codehaus.modello.ModelloException;
+import org.codehaus.modello.ModelloRuntimeException;
+import org.codehaus.modello.plugin.AbstractModelloGenerator;
+import org.codehaus.modello.generator.java.javasource.JClass;
+import org.codehaus.modello.generator.java.javasource.JMethod;
+import org.codehaus.modello.generator.java.javasource.JParameter;
+import org.codehaus.modello.generator.java.javasource.JSourceCode;
+import org.codehaus.modello.generator.java.javasource.JSourceWriter;
+import org.codehaus.modello.model.Model;
+import org.codehaus.modello.model.ModelAssociation;
+import org.codehaus.modello.model.ModelClass;
+import org.codehaus.modello.model.ModelDefault;
+import org.codehaus.modello.model.ModelField;
+import org.codehaus.modello.plugins.xml.XmlAssociationMetadata;
+import org.codehaus.modello.plugins.xml.XmlFieldMetadata;
+import org.codehaus.plexus.velocity.VelocityComponent;
+import org.apache.velocity.context.Context;
+import org.apache.velocity.VelocityContext;
+
+/**
+ * @author <a href="mailto:jason@modello.org">Jason van Zyl</a>
+ * @author <a href="mailto:evenisse@codehaus.org">Emmanuel Venisse</a>
+ * @version $Id$
+ */
+public class VelocityGenerator
+    extends AbstractModelloGenerator
+{
+    private VelocityComponent velocity;
+
+    public void generate( Model model, Properties parameters )
+        throws ModelloException
+    {
+        //initialize( model, parameters );
+
+        // Take the template from the properties
+        // Put the model in the context
+        // Generate        
+
+        try
+        {
+            Context context = new VelocityContext();
+
+            context.put( "model", model );
+
+            Writer writer = new FileWriter( "foo.txt" );
+
+            velocity.getEngine().mergeTemplate( "/modello/templates/stash.vm", context, writer );
+
+            writer.flush();
+
+            writer.close();
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+}
