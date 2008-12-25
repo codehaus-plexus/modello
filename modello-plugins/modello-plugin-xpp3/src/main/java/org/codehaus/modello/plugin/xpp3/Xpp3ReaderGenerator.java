77a0da510631001c32c3a98e705d3540df85fdb3
hboutemy <hboutemy@f0865d36-f2f9-0310-a560-dfd520db29d7>
2008-12-25 22:22:07 +0000

    code simplification

--- modello-plugins/modello-plugin-xpp3/src/main/java/org/codehaus/modello/plugin/xpp3/Xpp3ReaderGenerator.java
+++ modello-plugins/modello-plugin-xpp3/src/main/java/org/codehaus/modello/plugin/xpp3/Xpp3ReaderGenerator.java
@@ -1204,103 +1204,40 @@ public class Xpp3ReaderGenerator
 
         // --------------------------------------------------------------------
 
-        method = new JMethod( "getIntegerValue", JType.INT, null );
-        method.addException( new JClass( "XmlPullParserException" ) );
-        method.getModifiers().makePrivate();
-
-        method.addParameter( new JParameter( new JClass( "String" ), "s" ) );
-        method.addParameter( new JParameter( new JClass( "String" ), "attribute" ) );
-        method.addParameter( new JParameter( new JClass( "XmlPullParser" ), "parser" ) );
-        method.addParameter( new JParameter( JClass.BOOLEAN, "strict" ) );
-
-        sc = method.getSourceCode();
-
-        convertNumericalType( sc, "Integer.valueOf( s ).intValue()", "an integer" );
+        method = convertNumericalType( "getIntegerValue", JType.INT, "Integer.valueOf( s ).intValue()", "an integer" );
 
         jClass.addMethod( method );
 
         // --------------------------------------------------------------------
 
-        method = new JMethod( "getShortValue", JType.SHORT, null );
-        method.addException( new JClass( "XmlPullParserException" ) );
-        method.getModifiers().makePrivate();
-
-        method.addParameter( new JParameter( new JClass( "String" ), "s" ) );
-        method.addParameter( new JParameter( new JClass( "String" ), "attribute" ) );
-        method.addParameter( new JParameter( new JClass( "XmlPullParser" ), "parser" ) );
-        method.addParameter( new JParameter( JClass.BOOLEAN, "strict" ) );
-
-        sc = method.getSourceCode();
-
-        convertNumericalType( sc, "Short.valueOf( s ).shortValue()", "a short integer" );
+        method = convertNumericalType( "getShortValue", JType.SHORT, "Short.valueOf( s ).shortValue()",
+                                       "a short integer" );
 
         jClass.addMethod( method );
 
         // --------------------------------------------------------------------
 
-        method = new JMethod( "getByteValue", JType.BYTE, null );
-        method.addException( new JClass( "XmlPullParserException" ) );
-        method.getModifiers().makePrivate();
-
-        method.addParameter( new JParameter( new JClass( "String" ), "s" ) );
-        method.addParameter( new JParameter( new JClass( "String" ), "attribute" ) );
-        method.addParameter( new JParameter( new JClass( "XmlPullParser" ), "parser" ) );
-        method.addParameter( new JParameter( JClass.BOOLEAN, "strict" ) );
-
-        sc = method.getSourceCode();
-
-        convertNumericalType( sc, "Byte.valueOf( s ).byteValue()", "a byte" );
+        method = convertNumericalType( "getByteValue", JType.BYTE, "Byte.valueOf( s ).byteValue()", "a byte" );
 
         jClass.addMethod( method );
 
         // --------------------------------------------------------------------
 
-        method = new JMethod( "getLongValue", JType.LONG, null );
-        method.addException( new JClass( "XmlPullParserException" ) );
-        method.getModifiers().makePrivate();
-
-        method.addParameter( new JParameter( new JClass( "String" ), "s" ) );
-        method.addParameter( new JParameter( new JClass( "String" ), "attribute" ) );
-        method.addParameter( new JParameter( new JClass( "XmlPullParser" ), "parser" ) );
-        method.addParameter( new JParameter( JClass.BOOLEAN, "strict" ) );
-
-        sc = method.getSourceCode();
-
-        convertNumericalType( sc, "Long.valueOf( s ).longValue()", "a long integer" );
+        method = convertNumericalType( "getLongValue", JType.LONG, "Long.valueOf( s ).longValue()", "a long integer" );
 
         jClass.addMethod( method );
 
         // --------------------------------------------------------------------
 
-        method = new JMethod( "getFloatValue", JType.FLOAT, null );
-        method.addException( new JClass( "XmlPullParserException" ) );
-        method.getModifiers().makePrivate();
-
-        method.addParameter( new JParameter( new JClass( "String" ), "s" ) );
-        method.addParameter( new JParameter( new JClass( "String" ), "attribute" ) );
-        method.addParameter( new JParameter( new JClass( "XmlPullParser" ), "parser" ) );
-        method.addParameter( new JParameter( JClass.BOOLEAN, "strict" ) );
-
-        sc = method.getSourceCode();
-
-        convertNumericalType( sc, "Float.valueOf( s ).floatValue()", "a floating point number" );
+        method = convertNumericalType( "getFloatValue", JType.FLOAT, "Float.valueOf( s ).floatValue()",
+                                       "a floating point number" );
 
         jClass.addMethod( method );
 
         // --------------------------------------------------------------------
 
-        method = new JMethod( "getDoubleValue", JType.DOUBLE, null );
-        method.addException( new JClass( "XmlPullParserException" ) );
-        method.getModifiers().makePrivate();
-
-        method.addParameter( new JParameter( new JClass( "String" ), "s" ) );
-        method.addParameter( new JParameter( new JClass( "String" ), "attribute" ) );
-        method.addParameter( new JParameter( new JClass( "XmlPullParser" ), "parser" ) );
-        method.addParameter( new JParameter( JClass.BOOLEAN, "strict" ) );
-
-        sc = method.getSourceCode();
-
-        convertNumericalType( sc, "Double.valueOf( s ).doubleValue()", "a floating point number" );
+        method = convertNumericalType( "getDoubleValue", JType.DOUBLE, "Double.valueOf( s ).doubleValue()",
+                                       "a floating point number" );
 
         jClass.addMethod( method );
 
@@ -1406,8 +1343,19 @@ public class Xpp3ReaderGenerator
         jClass.addMethod( method );
     }
 
-    private void convertNumericalType( JSourceCode sc, String expression, String typeDesc )
+    private JMethod convertNumericalType( String methodName, JType returnType, String expression, String typeDesc )
     {
+        JMethod method = new JMethod( methodName, returnType, null );
+        method.addException( new JClass( "XmlPullParserException" ) );
+        method.getModifiers().makePrivate();
+
+        method.addParameter( new JParameter( new JClass( "String" ), "s" ) );
+        method.addParameter( new JParameter( new JClass( "String" ), "attribute" ) );
+        method.addParameter( new JParameter( new JClass( "XmlPullParser" ), "parser" ) );
+        method.addParameter( new JParameter( JClass.BOOLEAN, "strict" ) );
+
+        JSourceCode sc = method.getSourceCode();
+
         sc.add( "if ( s != null )" );
 
         sc.add( "{" );
@@ -1438,5 +1386,7 @@ public class Xpp3ReaderGenerator
         sc.add( "}" );
 
         sc.add( "return 0;" );
+
+        return method;
     }
 }
