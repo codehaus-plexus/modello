5003d705971872561d330974ae99de1a6d1c29c9
hboutemy <hboutemy@f0865d36-f2f9-0310-a560-dfd520db29d7>
2009-01-10 22:03:51 +0000

    added a test of multiple version generation with version in package

---
+++ modello-plugins/modello-plugin-java/src/test/java/org/codehaus/modello/plugin/java/FeaturesVersionJavaGeneratorTest.java
@@ -0,0 +1,70 @@
+package org.codehaus.modello.plugin.java;
+
+/*
+ * Copyright (c) 2004, Codehaus.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import org.codehaus.modello.AbstractModelloJavaGeneratorTest;
+import org.codehaus.modello.ModelloParameterConstants;
+import org.codehaus.modello.core.ModelloCore;
+import org.codehaus.modello.model.Model;
+
+import java.util.Properties;
+
+/**
+ * @author Hervé Boutemy
+ * @version $Id$
+ */
+public class FeaturesVersionJavaGeneratorTest
+    extends AbstractModelloJavaGeneratorTest
+{
+    public FeaturesVersionJavaGeneratorTest()
+    {
+        super( "features-version" );
+    }
+
+    private static final String ALL_VERSIONS = "1.0.0,1.5.0,2.0.0,3.0.0";
+
+    public void testJavaGenerator()
+        throws Throwable
+    {
+        ModelloCore modello = (ModelloCore) lookup( ModelloCore.ROLE );
+
+        Properties parameters = new Properties();
+        parameters.setProperty( ModelloParameterConstants.OUTPUT_DIRECTORY, getOutputDirectory().getAbsolutePath() );
+        parameters.setProperty( ModelloParameterConstants.ALL_VERSIONS, ALL_VERSIONS );
+        parameters.setProperty( ModelloParameterConstants.PACKAGE_WITH_VERSION, Boolean.toString( true ) );
+
+        Model model = modello.loadModel( getModelResource( "/features.mdo" ) );
+
+        String[] versions = ALL_VERSIONS.split( "," );
+
+        for ( int i = 0; i < versions.length; i++ )
+        {
+            parameters.setProperty( ModelloParameterConstants.VERSION, versions[i] );
+            modello.generate( model, "java", parameters );
+        }
+
+        compile( getOutputDirectory(), getOutputClasses() );
+
+        verify( "JavaVerifier", "java" );
+    }
+}
