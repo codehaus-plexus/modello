205f456d093a59bf715c3dadeb79c42a7f3c8639
hboutemy <hboutemy@f0865d36-f2f9-0310-a560-dfd520db29d7>
2009-10-16 00:53:03 +0000

    added Set unit test to existing List one

--- modello-plugins/modello-plugin-java/src/test/verifiers/interfaceAssociationTest/InterfaceAssociationVerifier.java
+++ modello-plugins/modello-plugin-java/src/test/verifiers/interfaceAssociationTest/InterfaceAssociationVerifier.java
@@ -5,8 +5,10 @@ import org.codehaus.modello.ifaceassociation.package1.Person;
 import org.codehaus.modello.ifaceassociation.package1.Location;
 import org.codehaus.modello.verifier.Verifier;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class InterfaceAssociationVerifier
     extends Verifier
@@ -17,12 +19,22 @@ public class InterfaceAssociationVerifier
 
         IPerson person = new Person();
 
+        // check List<IPerson> persons attribute getters/setters
         location.addPerson( person );
 
         List<IPerson> persons = location.getPersons();
 
         location.setPersons( new ArrayList<IPerson>( persons ) );
-        
+
         location.removePerson( person );
+
+        // check Set<IPerson> relatives attribute getters/setters
+        location.addRelative( person );
+
+        Set<IPerson> relatives = location.getRelatives();
+
+        location.setRelatives( new HashSet<IPerson>( relatives ) );
+
+        location.removeRelative( person );
     }
 }
