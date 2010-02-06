b6743db858cf36dd2548b3be7c588221a4012130
hboutemy <hboutemy@f0865d36-f2f9-0310-a560-dfd520db29d7>
2010-02-06 13:54:59 +0000

    [MODELLO-214] check that annotations aren't added to generated code when Java5 option is not enabled

---
+++ modello-plugins/modello-plugin-java/src/test/java/org/codehaus/modello/plugin/java/AnnotationsJava4GeneratorTest.java
@@ -0,0 +1,64 @@
+package org.codehaus.modello.plugin.java;
+
+/*
+ * Copyright (c) 2004, Codehaus.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import java.util.Properties;
+
+import org.codehaus.modello.AbstractModelloJavaGeneratorTest;
+import org.codehaus.modello.ModelloParameterConstants;
+import org.codehaus.modello.core.ModelloCore;
+import org.codehaus.modello.model.Model;
+
+/**
+ * Check that annotations are not added to generated sources when Java 5 features are not enabled.
+ *
+ * @version $Id$
+ */
+public class AnnotationsJava4GeneratorTest
+    extends AbstractModelloJavaGeneratorTest
+{
+    public AnnotationsJava4GeneratorTest()
+    {
+        super( "annotations-java4" );
+    }
+
+    public void testJava4GeneratorWithAnnotations()
+        throws Throwable
+    {
+        ModelloCore modello = (ModelloCore) lookup( ModelloCore.ROLE );
+
+        Model model = modello.loadModel( getXmlResourceReader( "/models/annotations.mdo" ) );
+
+        Properties parameters = new Properties();
+        parameters.setProperty( ModelloParameterConstants.OUTPUT_DIRECTORY, getOutputDirectory().getAbsolutePath() );
+        parameters.setProperty( ModelloParameterConstants.PACKAGE_WITH_VERSION, Boolean.toString( false ) );
+        parameters.setProperty( ModelloParameterConstants.VERSION, "1.0.0" );
+        parameters.setProperty( ModelloParameterConstants.USE_JAVA5, Boolean.toString( false ) );
+
+        modello.generate( model, "java", parameters );
+
+        addDependency( "javax.xml.bind", "jaxb-api", "2.1" );
+        addDependency( "org.apache.geronimo.specs", "geronimo-jpa_2.0_spec", "1.0" );
+        compile( getOutputDirectory(), getOutputClasses(), false );
+    }
+}
