76175197dc8384fb485085756893a8a8e59f02f4
hboutemy <hboutemy@f0865d36-f2f9-0310-a560-dfd520db29d7>
2010-02-13 17:50:22 +0000

    [MODELLO-232] removed unused code in verifier package

--- modello-test/src/main/java/org/codehaus/modello/verifier/Verifier.java
+++ modello-test/src/main/java/org/codehaus/modello/verifier/Verifier.java
@@ -25,8 +25,6 @@ package org.codehaus.modello.verifier;
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 import junit.framework.Assert;
 
@@ -37,118 +35,6 @@ public abstract class Verifier
 {
     public abstract void verify()
         throws Throwable;
-
-    public static void assertField( String className, String fieldName, Object expected, Object actual )
-    {
-        Method expectedMethod;
-
-        Class expectedValueType;
-
-        Class expectedClass = expected.getClass();
-
-        Class actualClass = actual.getClass();
-
-        if ( !expectedClass.isAssignableFrom( actualClass ) )
-        {
-            throw new WrongObjectTypeVerifierException( className, fieldName, expectedClass.getName(), actualClass.getName() );
-        }
-
-        expectedMethod = getMethod( expected.getClass(), fieldName );
-
-        expectedValueType = expectedMethod.getReturnType();
-
-        if ( expectedValueType.isPrimitive() )
-        {
-            throw new VerifierException( "This tester cannot test primitive types." );
-        }
-
-        if ( expectedValueType.isArray() )
-        {
-            throw new VerifierException( "This tester cannot test primitive types." );
-        }
-
-        if ( !expectedValueType.getName().equals( "java.lang.String" ) )
-        {
-            throw new VerifierException( "This tester cannot test any object other than java.lang.String." );
-        }
-
-        Method actualMethod = getMethod( actualClass, fieldName );
-
-        Class actualValueType = actualMethod.getReturnType();
-
-        // assert that the actual type is the same as the expected type
-        // or at least assignable from it.
-        if ( !actualValueType.isAssignableFrom( expectedValueType ) )
-        {
-            throw new WrongReturnTypeVerifierException( className, fieldName, expectedValueType.getName(), actualValueType.getName() );
-        }
-
-        Object expectedValue;
-
-        Object actualValue;
-
-        try
-        {
-            expectedValue = expectedMethod.invoke( expected, new Object[ 0 ] );
-
-            actualValue = actualMethod.invoke( actual, new Object[ 0 ] );
-        }
-        catch( IllegalAccessException ex )
-        {
-            throw new VerifierException( "Exception while calling method.", ex );
-        }
-        catch( InvocationTargetException ex )
-        {
-            throw new VerifierException( "Exception while calling method.", ex );
-        }
-
-        if ( expectedValue == null )
-        {
-            if ( actualValue == null )
-            {
-                return;
-            }
-
-            throw new NotEqualsVerifierException( className, fieldName, expectedValue, actualValue );
-        }
-
-        if ( !expectedValue.equals( actualValue ) )
-        {
-            throw new NotEqualsVerifierException( className, fieldName, expectedValue, actualValue );
-        }
-    }
-
-    private static Method getMethod( Class clazz, String fieldName )
-    {
-        String methodSuffix = Character.toUpperCase( fieldName.charAt( 0 ) ) + fieldName.substring( 1 );
-
-        try
-        {
-            Method method = clazz.getMethod( "get" + methodSuffix, new Class[ 0 ] );
-            if ( method.getReturnType() != Boolean.class )
-            {
-                return method;
-            }
-        }
-        catch( NoSuchMethodException ex )
-        {
-            // continue on
-        }
-
-        try
-        {
-            Method method = clazz.getMethod( "is" + methodSuffix, new Class[ 0 ] );
-            if ( method.getReturnType() == Boolean.class )
-            {
-                return method;
-            }
-        }
-        catch( NoSuchMethodException ex )
-        {
-            // continue on
-        }
-        throw new VerifierException( "The expected class doesn't have a field named '" + fieldName + "'." );
-    }
 
     protected File getTestFile( String name )
     {
