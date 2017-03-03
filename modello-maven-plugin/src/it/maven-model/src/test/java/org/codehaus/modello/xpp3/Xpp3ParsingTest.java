dd07bc47fb41e402cf0fbb2a5d58ae84fc47c0f1
rfscholte <rfscholte@apache.org>
2017-03-03 23:05:06 +0100

    Move maven-model IT to position where it is actually executed

--- modello-maven-plugin/src/it/maven-model/src/test/java/org/codehaus/modello/xpp3/Xpp3ParsingTest.java
+++ modello-maven-plugin/src/it/maven-model/src/test/java/org/codehaus/modello/xpp3/Xpp3ParsingTest.java
@@ -1,18 +1,29 @@
 package org.codehaus.modello.xpp3;
 
-import junit.framework.TestCase;
+import static org.hamcrest.CoreMatchers.startsWith;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.ReaderFactory;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.hamcrest.core.StringStartsWith;
 
 import java.io.File;
 
 /** @author Jason van Zyl */
 public class Xpp3ParsingTest
-    extends TestCase
 {
+    @Rule
+    public ExpectedException thrown= ExpectedException.none();
+
+    @Test
     public void testXpp3ParsingWithModelWithWrongRootTag()
         throws Exception
     {
+        thrown.expectMessage( startsWith( "Expected root element 'project' but found 'model' (position: START_TAG seen <model>... @1:7)" ) );
+        
         File model = new File( System.getProperty( "basedir" ), "src/test/models/model-with-wrong-root-tag.xml" );
 
         MavenXpp3Reader reader = new MavenXpp3Reader();
@@ -20,9 +31,12 @@ public class Xpp3ParsingTest
         reader.read( ReaderFactory.newXmlReader( model ), true );
     }
 
+    @Test
     public void testXpp3ParsingWithModelWithMissingElements()
         throws Exception
     {
+        thrown.expectMessage( startsWith( "Unrecognised tag: 'groupId' (position: START_TAG seen ...<dependencies>" ) );
+        
         File model = new File( System.getProperty( "basedir" ), "src/test/models/model-with-missing-elements.xml" );
 
         MavenXpp3Reader reader = new MavenXpp3Reader();
