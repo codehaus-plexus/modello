694dd2ff4ca68b19cbc8c5cd05c22b6507228c77
Sandra Parsick <sandra@parsick.dev>
2025-10-09 16:17:12 +0200

    chore: migrate junit 3 test to junit4
    Signed-off-by: Sandra Parsick <sandra@parsick.dev>

--- modello-plugins/modello-plugin-java/src/test/java/org/codehaus/modello/plugin/java/javasource/JMapTypeTest.java
+++ modello-plugins/modello-plugin-java/src/test/java/org/codehaus/modello/plugin/java/javasource/JMapTypeTest.java
@@ -1,25 +1,30 @@
 package org.codehaus.modello.plugin.java.javasource;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author <a href="mailto:simonetripodi@apache.org">Simone Tripodi</a>
  * @since 1.8
  */
-public final class JMapTypeTestCase extends TestCase {
+public final class JMapTypeTest {
 
+    @Test
     public void testJavaPropertiesType() {
         JMapType mapType = new JMapType("java.util.Properties", new JClass("String"));
 
         assertEquals("java.util.Properties", mapType.toString());
     }
 
+    @Test
     public void testJavaMapType() {
         JMapType mapType = new JMapType("java.util.Map", new JClass("String"));
 
         assertEquals("java.util.Map<Object, String>", mapType.toString());
     }
 
+    @Test
     public void testJavaMapInitialization() {
         JMapType mapType = new JMapType("java.util.Map", "new java.util.HashMap()", new JClass("String"));
 
